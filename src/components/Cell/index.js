cf63a0295c5939de2d95ea3add07d5d3b8802182
Nguyễn Văn Đại <37271151+nguyenvandai61@users.noreply.github.com>
2021-01-23 16:55:00 +0700

    Show a cell with a specified conditions

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './style.css';
 
 export class Cell extends Component {
     constructor(props) {
@@ -7,18 +8,78 @@ export class Cell extends Component {
             nBomb: 0,
             isBomb: false,
             isFlag: false,
-            isClicked: false,
-            isFinish : false
+            isOpened: false,
+
+            content: "", 
         }
     }
-    render() {
-        let content = "";
-        let {nBomb, isBomb, isFlag, isClicked} = this.state;
+    
+
+    onGame = () => {
+        let {isOpened} = this.state;
         
+        if (!isOpened) {
+            this.onOpenHandler();
+        } 
+        else {
+            this.onCloseHandler();
+        }
+    }
+    
+    onCloseHandler = () => {
+        this.setState({content: ""});    
+    } 
+
+    onOpenHandler = () => {
+        let {isBomb, nBomb} = this.state;
+        if (isBomb) {
+            this.onFinishHandler();
+        }
+        this.setState({content: nBomb});
+    }
+
+    onFinishHandler = () => {
+        let {isBomb, nBomb, isOpened, isFlag} = this.state;
+        // Case Lose:
+        let content = "";
+        if (!isBomb) {
+            content = nBomb;
+            if (isFlag) {
+                content = "M";
+            }
+        } else {
+            content = "B";
+            if (isOpened) {
+                content = "*";
+            }    
+        }
+        this.setState({content: content});    
+    }
+    //*************** */
+    // User actions
+    //************** */
+    openCell = () => {
+        if (this.state.isOpened) return;
+        console.log("Open cell");
+        this.setState({isOpened: true});
+        this.onOpenHandler();
+    }
+
+    flagCell = (e) => {
+        e.preventDefault(); // Prevent open context menu
+        this.setState({isFlag: true, content: "F"});
+    }
+
+    render() {
+        let {content} = this.state;
         
         return (
-            <div style={{backgroundColor: "#9A8598"}}>
-                <button>{content}</button>
+            <div 
+            className="cell" 
+            style={{backgroundColor: "#9A8598"}} 
+            onClick={this.openCell}
+            onContextMenu={this.flagCell}>
+                <span>{content}</span>
             </div>
         )
     }
