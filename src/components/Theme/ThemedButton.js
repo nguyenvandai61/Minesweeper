90ebee6f328d8ca07489ac1d3b91eb5268b81295
DaiNguyen <37271151+nguyenvandai61@users.noreply.github.com>
2021-02-07 00:34:41 +0700

    Use ContextAPI set up restart board

---
+++ src/components/Theme/ThemedButton.js
@@ -0,0 +1,16 @@
+import {ThemeContext} from "./ThemeContext"
+import {Component} from 'react'
+
+class ThemedButton extends Component {
+    render() {
+        let props = this.props;
+        let theme = this.context;
+        return (
+            <button {...props} style={{backgroundColor: theme.background}}/>
+        )
+    }
+}
+
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
